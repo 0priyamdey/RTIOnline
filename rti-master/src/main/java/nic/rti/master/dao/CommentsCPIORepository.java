53f48efcaf43da780eea5f69688dd6c44dba5ffb
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-14 17:32:46 +0530

    Update CommentsCPIORepository.java

--- rti-master/src/main/java/nic/rti/master/dao/CommentsCPIORepository.java
+++ rti-master/src/main/java/nic/rti/master/dao/CommentsCPIORepository.java
@@ -1,6 +1,6 @@
 package nic.rti.master.repository;
 
-import com.example.piodatech.entity.CommentsCPIO;
+import nic.rti.master.entity.CommentsCPIO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;

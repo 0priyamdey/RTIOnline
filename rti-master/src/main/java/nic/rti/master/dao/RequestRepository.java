d1bbf128b4a9170702175ec4b5c898b12b344f3b
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-14 17:08:16 +0530

    Update RequestRepository.java

--- rti-master/src/main/java/nic/rti/master/dao/RequestRepository.java
+++ rti-master/src/main/java/nic/rti/master/dao/RequestRepository.java
@@ -1,6 +1,6 @@
 package nic.rti.master.repository;
 
-import com.example.piodatech.entity.Request;
+import nic.rti.master.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;

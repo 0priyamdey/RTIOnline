9a10fabe79dc61cd7a68b25cdfd5a15c1869ab56
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-14 16:45:31 +0530

    Create CommentsCPIORepository.java

---
+++ rti-master/src/main/java/nic/rti/master/dao/CommentsCPIORepository.java
@@ -0,0 +1,17 @@
+package nic.rti.master.repository;
+
+import com.example.piodatech.entity.CommentsCPIO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentsCPIORepository extends JpaRepository<CommentsCPIO, String> {
+
+    @Query(value = "SELECT COUNT(A.registration_no) FROM \"RTIMIS\".\"appeal\" A NATURAL JOIN \"RTIMIS\".\"commentscpio\" C WHERE A.cpio_app = :pioId AND A.org_code = :orgCode AND A.closing_date IS NULL AND C.flag = 'S'", nativeQuery = true)
+    Long countCommentsSoughtByAA(@Param("pioId") Integer pioId, @Param("orgCode") String orgCode);
+
+    @Query(value = "SELECT COUNT(DISTINCT A.registration_no) FROM \"RTIMIS\".\"commentscpio\" C JOIN \"RTIMIS\".\"appeal\" A ON C.registration_no = A.registration_no WHERE C.flag = 'R' AND A.closing_date IS NULL AND A.cpio_app = :pioId", nativeQuery = true)
+    Long countCommentsGivenByPIO(@Param("pioId") Integer pioId);
+}

0a1339d26fda2ac83972fb5e90cf143398c743a8
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-14 16:14:32 +0530

    Create PioAppeal.java

---
+++ rti-master/src/main/java/nic/rti/master/entity/PioAppeal.java
@@ -0,0 +1,31 @@
+package nic.rti.master.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "appeal", schema = "rtimis")
+@Data
+public class Appeal {
+
+    @Id
+    @Column(name = "registration_no")
+    private String registrationNo;
+
+    @Column(name = "request_no")
+    private String requestNo;
+
+    @Column(name = "cpio_app")
+    private Integer cpioApp;
+
+    @Column(name = "org_code")
+    private String orgCode;
+
+    @Column(name = "closing_date")
+    private LocalDate closingDate;
+
+}
